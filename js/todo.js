46a5c06d058380c37c57f3255bba00ad73bb0e7e
USER <USER>
2016-06-03 10:19:21 +0800

    -jQuery

---
+++ js/todo.js
@@ -0,0 +1,18 @@
+/*function kpress(event){
+	var in_list = document.getElementById("in_list");
+	var ul_list = document.getElementById("list");
+	var in_list_val = in_list.value;
+
+	if (event.keyCode==13) {
+		//console.log(in_list_val);
+		if (in_list_val == '') {
+			alert("please write a list");
+		} else {
+			ul_list.innerHTML += "<li>"+in_list_val+"</li>";
+			clear(in_list);
+		}
+	}
+}
+function clear(in_list){
+	if(in_list)
+}*/
